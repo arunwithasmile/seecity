cdfc17ba07147a681c225e991f41325e80a80d89
arun <arun@localhost>
2022-10-27 02:03:05 +0530

    Changed JWT Provider

--- backend/src/main/java/com/arun/seecity/security/JwtHelperService.java
+++ backend/src/main/java/com/arun/seecity/security/JwtHelperService.java
@@ -3,83 +3,36 @@
  */
 package com.arun.seecity.security;
 
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 
 /**
  * @author arun
  *
  */
 @Service
-public class JwtHelperService {
+public interface JwtHelperService {
 
-	private static final String secret = "(ityS3cret";
-	private static final int EXPIRE_TIME_HRS = 0;
+	static final String secret = "(ityS3cret";
+	static final int EXPIRE_TIME_HRS = 8;
 
-	@Autowired
-	private UserDetailsService userDetailsService;
+	public String generateToken(UserDetails userDetails, List<String> roles);
 
-	private Claims getClaims(String jwt) {
-		return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(jwt).getBody();
-	}
+	public boolean isValid(String token);
 
-	public String generateToken(UserDetails userDetails, List<String> roles) {
-		Map<String, Object> claims = new HashMap<>();
-		claims.put("roles", claims);
-		String userName = userDetails.getUsername();
-		JwtBuilder builder = Jwts.builder();
-		builder = builder.setClaims(claims);
-		builder = builder.setSubject(userName);
-		builder = builder.setIssuedAt(new Date());
-		builder = builder.signWith(SignatureAlgorithm.HS256, secret);
-		String token = builder.compact();
-		return token;
-	}
-
-	public boolean isValid(String jwt) {
-		if (jwt == null || jwt.isEmpty()) {
-			return false;
-		}
-		try {
-			Claims claims = getClaims(jwt);
-			String userName = (String) claims.get(Claims.SUBJECT);
-			UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
-			return userDetails != null;
-		} catch (ExpiredJwtException e) {
-			return false;
-		}
-	}
-
-	public String resolveToken(HttpServletRequest request) {
+	default String resolveToken(HttpServletRequest request) {
 		final String autorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-		if (autorizationHeader != null && autorizationHeader.startsWith("Bearer ")) {
+		if (autorizationHeader != null && autorizationHeader.startsWith("Bearer")) {
 			return autorizationHeader.substring(7);
 		}
 		return null;
 	}
 
-	public Authentication getAuthentication(String jwt) {
-		Claims claims = getClaims(jwt);
-		String userName = (String) claims.get(Claims.SUBJECT);
-		UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
-		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
-	}
+	public Authentication getAuthentication(String token);
 }
---
+++ backend/src/main/java/com/arun/seecity/security/JwtHelperServiceImpl.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package com.arun.seecity.security;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+/**
+ * @author arun
+ *
+ */
+@Service
+//@Primary
+public class JwtHelperServiceImpl implements JwtHelperService {
+
+	private static final String secret = "(ityS3cret";
+	private static final int EXPIRE_TIME_HRS = 8;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	private Claims getClaims(String jwt) {
+		return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(jwt).getBody();
+	}
+
+	public String generateToken(UserDetails userDetails, List<String> roles) {
+		String userName = userDetails.getUsername();
+		Date now = new Date();
+		Claims claims = Jwts.claims().setSubject(userName);
+		claims.put("roles", claims);
+		JwtBuilder builder = Jwts.builder().setClaims(claims).setIssuedAt(now)
+				.setExpiration(new Date(now.getTime() + EXPIRE_TIME_HRS * 60 * 60 * 1000))
+				.signWith(SignatureAlgorithm.HS256, Base64.getEncoder().encode(secret.getBytes()));
+		String token = builder.compact();
+		return token;
+	}
+
+	public boolean isValid(String jwt) {
+		if (jwt == null || jwt.isEmpty()) {
+			return false;
+		}
+		try {
+			Claims claims = getClaims(jwt);
+			String userName = (String) claims.get(Claims.SUBJECT);
+			UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+			return userDetails != null;
+		} catch (ExpiredJwtException e) {
+			return false;
+		}
+	}
+
+	public Authentication getAuthentication(String jwt) {
+		Claims claims = getClaims(jwt);
+		String userName = (String) claims.get(Claims.SUBJECT);
+		UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+		return new UsernamePasswordAuthenticationToken(userName, "", userDetails.getAuthorities());
+	}
+}
